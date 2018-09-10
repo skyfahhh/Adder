03063081683731ee0f9ba5d08a05bf48808ed05b
skyfah <60050155@kmitl.a.th>
2018-09-10 18:47:58 +0700

    add : adder java

---
+++ Adder.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Adder {
+    public static void main(String[] args) {
+      Scanner sc = new Scanner (System.in);
+      System.out.print("Enter x ="); 
+      int x = sc.nextInt();
+      System.out.print("Enter y ="); 
+      int y = sc.nextInt();
+      int z = 0;
+      z = x+y;
+      System.out.println(z);
+    }
+}
